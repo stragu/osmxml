23ec773bf8a68776ac19181b4a85f7dc781b0750
Stéphane Guillou <stephane.guillou@member.fsf.org>
2022-04-16 23:56:28 +1000

    custom class and with print method; citation skeleton; document; let roxygen 2 take care of NAMESPACE

--- R/read_osm.R
+++ R/read_osm.R
@@ -4,14 +4,36 @@
 #'
 #' @param path Path to .osm file (string)
 #'
-#' @return An object of class "osm collection"
-#' @importFrom sf st_read
+#' @return An object of class "osm". There is a print method for this class.
+#' @importFrom sf st_read st_layers
 #' @export
 #'
 #' @examples
 read_osm <- function(path) {
-  if (!file.exists("R/hello.R")) stop("'path' is valid path to a .osm file")
-  osm_layers <- sapply(layer_names, function(x) st_read("inst/extdata/south_brisbane.osm", layer = x),
+  if (!file.exists(path)) stop("'path' is not a valid path to a .osm file")
+  # store names of all components of OSM file
+  layer_names <- st_layers(path)$name
+  # read all layers, keeping names
+  osm_layers <- sapply(layer_names, function(x) st_read(path,
+                                                        layer = x,
+                                                        quiet = TRUE),
          USE.NAMES = TRUE)
-  class(osm_layers) <- "osm collection"
+  # assign S3 class
+  class(osm_layers) <- "osm"
+  # return a list of class "osm collection"
+  osm_layers
+}
+
+
+#' Title
+#'
+#' @param obj
+#'
+#' @return
+#' @export
+#'
+#' @examples
+print.osm <- function(obj) {
+  cat("OSM data object made of", length(obj), "simple feature collections: ")
+  cat(paste(names(obj), collapse = ", "))
 }
--- testing.R
+++ testing.R
@@ -14,3 +14,11 @@ osm_all_layers <- sapply(layer_names, function(x) st_read("inst/extdata/south_br
 plot(osm_all_layers$points)
 class(osm_all_layers) <- "osm_collection"
 plot(osm_all_layers$multipolygons)
+
+# test read function
+south_brisbane <- read_osm("inst/extdata/south_brisbane.osm")
+south_brisbane
+class(south_brisbane)
+print.osm(south_brisbane)
+south_brisbane
+
