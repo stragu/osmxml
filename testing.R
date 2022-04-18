9137ac7e9368d4b7e24fe483b95f21bd32e77458
Stéphane Guillou <stephane.guillou@member.fsf.org>
2022-04-18 22:54:11 +1000

    organise thoughts

--- testing.R
+++ testing.R
@@ -1,3 +1,12 @@
+#### TODO ####
+
+# - get bbox at import so it can be used for first plotted layer
+# - support XML of single object (?) -> this would add osmdata to dependencies
+# - add tests
+# - make binding faster
+# - import id presets: https://github.com/openstreetmap/id-tagging-schema/tree/main/data/presets
+
+
 #### Example OSM file ####
 
 # downloaded from the website
@@ -31,10 +40,7 @@ south_brisbane$other_relations %>% st_drop_geometry() %>% View()
 # transparent grey
 rgb(198, 198, 198, 100, maxColorValue = 255)
 
-
-# TODO
-# - get bbox at import so it can be used for first plotted layer
-# - support XML of single object
+#### Faster ####
 
 # now very slow, with tag expansion
 south_brisbane <- read_osm("inst/extdata/south_brisbane.osm")
@@ -42,20 +48,24 @@ south_brisbane <- read_osm("inst/extdata/south_brisbane.osm",
                            expand_tags = FALSE)
 south_brisbane
 # test slowness
-x, col_name = "other_tags") {
-  # remove geometry to deal with simple dataframe
-  sf_attr <- sf::st_drop_geometry(x)
-  # split into list of single-row dataframes
-  sf_attr_split <- split(sf_attr, 1:nrow(sf_attr))
-  # separate other_tags for each
-  sf_attr_sep <- sapply(sf_attr_split, separate_tags_single, col_name = col_name)
-  # bind rows into single dataframe
-  all_names <- unique(unlist(lapply(sf_attr_sep, names)))
-  sf_attr_sep_all <- do.call(rbind,
-                             lapply(sf_attr_sep,
-                                    function(current_df) data.frame(c(current_df,
-                                                                      sapply(setdiff(all_names, names(current_df)),
-                                                                             function(y) NA)),
-                                                                    check.names = FALSE)))
-  # add geometries back in
-  sf::st_sf(st_geometry(x), sf_attr_sep_all)
+x = south_brisbane$points
+col_name = "other_tags"
+# remove geometry to deal with simple dataframe
+sf_attr <- sf::st_drop_geometry(x)
+# split into list of single-row dataframes
+sf_attr_split <- split(sf_attr, 1:nrow(sf_attr))
+# separate other_tags for each
+sf_attr_sep <- sapply(sf_attr_split, separate_tags_single, col_name = col_name)
+# bind rows into single dataframe
+all_names <- unique(unlist(lapply(sf_attr_sep, names)))
+# -> THIS IS SLOW
+sf_attr_sep_all <- do.call(rbind,
+                           lapply(sf_attr_sep,
+                                  function(current_df) data.frame(c(current_df,
+                                                                    sapply(setdiff(all_names, names(current_df)),
+                                                                           function(y) NA)),
+                                                                  check.names = FALSE)))
+# try bind_rows instead: so much faster...
+sf_attr_sep_all <- dplyr::bind_rows(sf_attr_sep)
+# add geometries back in
+sf::st_sf(st_geometry(x), sf_attr_sep_all)
